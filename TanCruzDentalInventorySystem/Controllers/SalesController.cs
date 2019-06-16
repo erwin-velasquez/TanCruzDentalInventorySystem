5bd8455c861623b792822f7fc7ce98952b73dfc4
mikodaray <mikodaray@gmail.com>
2019-06-16 12:20:58 +0800

    asdf

---
+++ TanCruzDentalInventorySystem/Controllers/SalesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TanCruzDentalInventorySystem.Controllers
+{
+    public class SalesController : Controller
+    {
+        // GET: Sales
+        public ActionResult SalesHome()
+        {
+            return View();
+        }
+
+        public ActionResult SalesDocument()
+        {
+            return View();
+        }
+    }
+}
