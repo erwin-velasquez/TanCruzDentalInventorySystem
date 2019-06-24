2e01bfdff6aaddbc951f70a69070b15919a40cae
jamespm01 <manglinongjames@gmail.com>
2019-06-24 13:42:16 +0800

    Purchase order list initial styling.

--- TanCruzDentalInventorySystem/Repository/PurchaseRepository.cs
+++ TanCruzDentalInventorySystem/Repository/PurchaseRepository.cs
@@ -7,27 +7,40 @@ using TanCruzDentalInventorySystem.Repository.DataServiceInterface;
 
 namespace TanCruzDentalInventorySystem.Repository
 {
-	public class PurchaseRepository : IPurchaseRepository
-	{
-		public IUnitOfWork UnitOfWork { get; set; }
+    public class PurchaseRepository : IPurchaseRepository
+    {
+        public IUnitOfWork UnitOfWork { get; set; }
 
-		public IEnumerable<Purchase> GetPurchaseList()
-		{
+        public IEnumerable<Purchase> GetPurchaseList()
+        {
             List<Purchase> result = new List<Purchase>();
-            result.Add(new Purchase() { BP_ID = "1", CHANGE_DATE = DateTime.UtcNow, CREATE_DATE = DateTime.UtcNow, REMARKS = "James" });
-            result.Add(new Purchase() { BP_ID = "2", CHANGE_DATE = DateTime.UtcNow, CREATE_DATE = DateTime.UtcNow, REMARKS = "Test 2" });
-            result.Add(new Purchase() { BP_ID = "3", CHANGE_DATE = DateTime.UtcNow, CREATE_DATE = DateTime.UtcNow, REMARKS = "Test 3" });
-            result.Add(new Purchase() { BP_ID = "4", CHANGE_DATE = DateTime.UtcNow, CREATE_DATE = DateTime.UtcNow, REMARKS = "Test 4" });
-            result.Add(new Purchase() { BP_ID = "5", CHANGE_DATE = DateTime.UtcNow, CREATE_DATE = DateTime.UtcNow, REMARKS = "Test 5" });
-            result.Add(new Purchase() { BP_ID = "6", CHANGE_DATE = DateTime.UtcNow, CREATE_DATE = DateTime.UtcNow, REMARKS = "Test 6" });
-            result.Add(new Purchase() { BP_ID = "7", CHANGE_DATE = DateTime.UtcNow, CREATE_DATE = DateTime.UtcNow, REMARKS = "Test 7" });
-            result.Add(new Purchase() { BP_ID = "8", CHANGE_DATE = DateTime.UtcNow, CREATE_DATE = DateTime.UtcNow, REMARKS = "Test 8" });
-            result.Add(new Purchase() { BP_ID = "9", CHANGE_DATE = DateTime.UtcNow, CREATE_DATE = DateTime.UtcNow, REMARKS = "Test 9" });
-            result.Add(new Purchase() { BP_ID = "10", CHANGE_DATE = DateTime.UtcNow, CREATE_DATE = DateTime.UtcNow, REMARKS = "Test 10" });
-            result.Add(new Purchase() { BP_ID = "11", CHANGE_DATE = DateTime.UtcNow, CREATE_DATE = DateTime.UtcNow, REMARKS = "Test 11" });
-            result.Add(new Purchase() { BP_ID = "12", CHANGE_DATE = DateTime.UtcNow, CREATE_DATE = DateTime.UtcNow, REMARKS = "Test 12" });
+            for (int x = 0; x < 100; x++)
+            {
+                result.Add(new Purchase()
+                {
+                    BP_ID = "bp_id " + x.ToString(),
+                    CHANGE_DATE = DateTime.UtcNow,
+                    CHANGE_ID = "Manglinong, James P.",
+                    CREATE_DATE = DateTime.UtcNow,
+                    CREATE_ID = "Manglinong, James P.",
+                    CURRENCY_ID = "PHP",
+                    DELIVERY_DATE = DateTime.UtcNow,
+                    DOCUMENT_DATE = DateTime.UtcNow,
+                    ID = x,
+                    POSTING_DATE = DateTime.UtcNow,
+                    PO_CONTROL_NUM = x,
+                    PO_DISCOUNT = 10,
+                    PO_DISC_AMT = 10,
+                    PO_STATUS = "Active",
+                    PURCHASEORDER_ID = x.ToString(),
+                    REFDOC_NUM = "REFDOC_NUM" + x.ToString(),
+                    REMARKS = "Purchase sampling 101. This is a test purchase.",
+                    SO_TAX = 1,
+                    SO_TOTAL = 100
+                }); ; ;
+            }
             IEnumerable<Purchase> output = result;
             return output;
         }
-	}
+    }
 }
