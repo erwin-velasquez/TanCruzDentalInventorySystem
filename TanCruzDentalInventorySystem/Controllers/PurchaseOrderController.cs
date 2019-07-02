17f423ff35ff02794eed5a6f00a2ced7e65df389
jamespm01 <manglinongjames@gmail.com>
2019-07-02 10:59:23 +0800

    Purchase order cleanup.

--- TanCruzDentalInventorySystem/Controllers/PurchaseOrderController.cs
+++ TanCruzDentalInventorySystem/Controllers/PurchaseOrderController.cs
@@ -6,32 +6,32 @@ using TanCruzDentalInventorySystem.ViewModels;
 
 namespace TanCruzDentalInventorySystem.Controllers
 {
-	[Authorize]
-	public class PurchaseOrderController : Controller
-	{
-		private IPurchaseOrderService _purchaseOrderService;
+    [Authorize]
+    public class PurchaseOrderController : Controller
+    {
+        private IPurchaseOrderService _purchaseOrderService;
 
-		public PurchaseOrderController(IPurchaseOrderService purchaseService)
-		{
-			_purchaseOrderService = purchaseService;
-		}
+        public PurchaseOrderController(IPurchaseOrderService purchaseService)
+        {
+            _purchaseOrderService = purchaseService;
+        }
 
-		public ActionResult Index()
-		{
-			return View();
-		}
+        public ActionResult Index()
+        {
+            return View();
+        }
 
-		public async Task<ActionResult> PurchaseOrderRecord(string purchaseOrderId)
-		{
-			var purchaseOrder = await _purchaseOrderService.GetPurchaseOrder(purchaseOrderId);
+        public async Task<ActionResult> PurchaseOrderRecord(string purchaseOrderId)
+        {
+            var purchaseOrder = await _purchaseOrderService.GetPurchaseOrder(purchaseOrderId);
 
-			// TODO: James to create the PurchaseOrder view
-			return View(purchaseOrder);
-		}
+            // TODO: James to create the PurchaseOrder view
+            return View(purchaseOrder);
+        }
 
-		[Authorize(Roles = "Editor")]
-		public async Task<ActionResult> CreatePurchaseOrder()
-		{
+        [Authorize(Roles = "Editor")]
+        public async Task<ActionResult> CreatePurchaseOrder()
+        {
             var purchaseOrderForm = await _purchaseOrderService.CreatePurchaseOrderForm(User.Identity.GetUserId());
 
             // TODO: James to create the PurchaseOrder create view
@@ -40,43 +40,44 @@ namespace TanCruzDentalInventorySystem.Controllers
 
         }
 
-		[Authorize(Roles = "Editor")]
-		public async Task<ActionResult> EditPurchaseOrderRecord(string purchaseOrderId)
-		{
-			var purchaseOrderForm = await _purchaseOrderService.GetPurchaseOrderForm(purchaseOrderId);
+        [Authorize(Roles = "Editor")]
+        public async Task<ActionResult> EditPurchaseOrderRecord(string purchaseOrderId)
+        {
+            var purchaseOrderForm = await _purchaseOrderService.GetPurchaseOrderForm(purchaseOrderId);
 
-			return View(purchaseOrderForm);
-		}
+            return View(purchaseOrderForm);
+        }
 
-		public async Task<ActionResult> GetPurchaseOrderList()
-		{
-			var purchaseOrders = await _purchaseOrderService.GetPurchaseOrderList();
+        public async Task<ActionResult> GetPurchaseOrderList()
+        {
+            var purchaseOrders = await _purchaseOrderService.GetPurchaseOrderList();
 
-			return Json(new { data = purchaseOrders }, JsonRequestBehavior.AllowGet);
-		}
+            return Json(new { data = purchaseOrders }, JsonRequestBehavior.AllowGet);
+        }
 
-		[HttpPost]
-		[Authorize(Roles = "Editor")]
-		[ValidateAntiForgeryToken]
-		public async Task<ActionResult> SavePurchaseOrderRecord(PurchaseOrderFormViewModel purchaseOrderForm)
-		{
-			if (ModelState.IsValid)
-			{
-				purchaseOrderForm.PurchaseOrder.UserId = User.Identity.GetUserId();
-				var recordsSaved = await _purchaseOrderService.SavePurchaseOrder(purchaseOrderForm.PurchaseOrder);
+        [HttpPost]
+        [Authorize(Roles = "Editor")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> SavePurchaseOrderRecord(PurchaseOrderFormViewModel purchaseOrderForm)
+        {
+            if (ModelState.IsValid)
+            {
+                purchaseOrderForm.PurchaseOrder.UserId = User.Identity.GetUserId();
+                var recordsSaved = await _purchaseOrderService.SavePurchaseOrder(purchaseOrderForm.PurchaseOrder);
 
-				if (recordsSaved >= 1)
-				{
-					var purchaseOrder = await _purchaseOrderService.GetPurchaseOrder(purchaseOrderForm.PurchaseOrder.PurchaseOrderId);
-					return View("PurchaseOrderRecord", purchaseOrder);
-				}
-				ModelState.AddModelError(string.Empty, "There was a problem and the PurchaseOrder was not saved.");
-			}
+                if (recordsSaved >= 1)
+                {
+                    var purchaseOrder = await _purchaseOrderService.GetPurchaseOrder(purchaseOrderForm.PurchaseOrder.PurchaseOrderId);
+                    return View("PurchaseOrderRecord", purchaseOrder);
+                }
+                ModelState.AddModelError(string.Empty, "There was a problem and the PurchaseOrder was not saved.");
+            }
 
-			purchaseOrderForm = await _purchaseOrderService.GetPurchaseOrderForm(purchaseOrderForm.PurchaseOrder.PurchaseOrderId);
+            purchaseOrderForm = await _purchaseOrderService.GetPurchaseOrderForm(purchaseOrderForm.PurchaseOrder.PurchaseOrderId);
 
-			// TODO: James to create the PurchaseOrder input view
-			return View("EditPurchaseOrderRecord", purchaseOrderForm);
-		}
-	}
+            // TODO: James to create the PurchaseOrder input view
+            return View("EditPurchaseOrderRecord", purchaseOrderForm);
+        }
+
+    }
 }
