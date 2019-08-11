1477a5d56279456e58fa89370ac4f842fdf354cc
erwin-velasquez <50427894+erwin-velasquez@users.noreply.github.com>
2019-08-12 01:59:43 +0800

    Added SalesOrderDetail initial classes, and updated SalesOrderDetail table definitions

---
+++ TanCruzDentalInventorySystem/Models/SalesOrderDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TanCruzDentalInventorySystem.Models
+{
+	public class SalesOrderDetail
+	{
+		public string SalesOrderDetailId { get; set; }
+		public string SalesOrderId { get; set; }
+		public string ItemId { get; set; }
+		public ItemPrice ItemPrice { get; set; }
+		public decimal Quantity { get; set; }
+		public decimal QuantityOnHand { get; set; }
+		public decimal SalesOrderDetailDiscount { get; set; }
+		public decimal SalesOrderDetailDiscountAmount { get; set; }
+		public string TaxId { get; set; }
+		public decimal SalesOrderDetailTax { get; set; }
+		public decimal SalesOrderDetailTotal { get; set; }
+		public string Remarks { get; set; }
+		public string UserId { get; set; }
+		public DateTime? ChangedDate { get; set; }
+		public long VersionTimeStamp { get; set; }
+	}
+}
---
+++ TanCruzDentalInventorySystem/Models/Tax.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TanCruzDentalInventorySystem.Models
+{
+	public class Tax
+	{
+		public string TaxId { get; set; }
+		public string TaxName { get; set; }
+		public string TaxDescription { get; set; }
+		public decimal TaxValue { get; set; }
+		public bool IsDefault { get; set; }
+		public string TaxStatus { get; set; }
+		public string UserId { get; set; }
+	}
+}
