723e5c5b3eaf1d96c7c0b96df5eb021a82f7e6ca
james manglinong <manglinongjames@gmail.com>
2019-08-22 19:02:39 +0800

    SalesOrderDetailViewModel cleanup

--- TanCruzDentalInventorySystem/ViewModels/SalesOrderDetailViewModel.cs
+++ TanCruzDentalInventorySystem/ViewModels/SalesOrderDetailViewModel.cs
@@ -20,19 +20,5 @@ namespace TanCruzDentalInventorySystem.ViewModels
 		public string UserId { get; set; }
 		public DateTime? ChangedDate { get; set; }
 		public long VersionTimeStamp { get; set; }
-
-		//public int SalesOrderDetails_ID { get; set; }
-		//public string SalesOrderDetails_LineID { get; set; }
-		//public string SalesOrderDetails_SalesOrderId { get; set; }
-		//public string SalesOrderDetails_ItemId { get; set; }
-		//public decimal SalesOrderDetails_ItemPrice { get; set; }
-		//      public decimal SalesOrderDetails_Quantity { get; set; }
-		//      public decimal SalesOrderDetails_OnHandQuantity { get; set; }
-		//      public decimal SalesOrderDetails_LineDiscount { get; set; }
-		//      public decimal SalesOrderDetails_LineDiscountAmount { get; set; }
-		//      public string SalesOrderDetails_TaxId { get; set; }
-		//      public decimal SalesOrderDetails_LineTaxAmount { get; set; }
-		//      public decimal SalesOrderDetails_LineTotal { get; set; }
-		//      public string SalesOrderDetails_Remarks { get; set; }
 	}
 }
