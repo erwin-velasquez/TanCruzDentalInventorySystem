ad39caf61105c6663510e1d4d02e359f4e8eef60
Miko <mikodaray@gmail.com>
2019-10-23 10:08:06 +0800

    with updates

---
+++ TanCruzDentalInventorySystem/Scripts/Items/ItemList.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+
+    var table = $('#itemHomeTable').DataTable({
+        "language": {
+            "processing": "loading....",
+            "emptyTable": "No data found"
+        },
+        select: {
+            style: 'os',
+            selector: 'td'
+        },
+        "paging": true,
+        "autoWidth": true,
+        "bLengthChange": false,
+        "bPaginate": false
+    });
+
+});
