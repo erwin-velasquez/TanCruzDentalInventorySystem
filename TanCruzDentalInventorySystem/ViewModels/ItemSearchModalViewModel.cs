c69bca5f124288ce439c562abaf8431185e2bd72
mikodaray <mikodaray@gmail.com>
2019-11-26 07:34:15 +0800

    fixed issue on item modal on purchasing

--- TanCruzDentalInventorySystem/ViewModels/ItemSearchModalViewModel.cs
+++ TanCruzDentalInventorySystem/ViewModels/ItemSearchModalViewModel.cs
@@ -6,14 +6,9 @@ namespace TanCruzDentalInventorySystem.ViewModels
 {
 		public class ItemSearchFormViewModel
 		{
+				public string ItemPriceDetailsItemId { get; set; }
 				public ItemViewModel Item { get; set; }
 				public ItemPriceViewModel SalesOrderItemPrice { get; set; }
 				public ItemPriceViewModel PurchaseOrderItemPrice { get; set; }
-				public IEnumerable<ItemGroupViewModel> ItemGroups { get; set; }
-				public IEnumerable<CurrencyViewModel> Currencies { get; set; }
-				public IEnumerable<UnitOfMeasureViewModel> UnitOfMeasures { get; set; }
-				public IEnumerable<BusinessPartnerViewModel> BusinessPartners { get; set; }
-				public IEnumerable<PurchasingUnitOfMeasureViewModel> PurchasingUnitOfMeasures { get; set; }
-				public IEnumerable<InventoryUnitOfMeasureViewModel> InventoryUnitOfMeasures { get; set; }
 		}
 }
