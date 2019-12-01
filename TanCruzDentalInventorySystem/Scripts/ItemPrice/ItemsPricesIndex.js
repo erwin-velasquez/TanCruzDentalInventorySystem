a932bcf4fc42b93a4215e3f5ac50d8e36099c6d1
james manglinong <manglinongjames@gmail.com>
2019-12-02 03:28:44 +0800

    Implemented item price nanagement view

---
+++ TanCruzDentalInventorySystem/Scripts/ItemPrice/ItemsPricesIndex.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+
+
+    $('#ItemsPricesListTable').DataTable({
+        "searching": false,
+        "language": {
+            "processing": "loading....",
+            "emptyTable": "No data found"
+        },
+        //"columnDefs": [{
+        //    "orderable": false,
+        //    "targets": 1,
+        //    "data": null,
+        //    "className": "center",
+        //    "defaultContent": '<a href="" class="editor_view">View</a>',
+        //    "width": "55px"
+        //}],
+        "paging": true,
+        "autoWidth": false,
+        "bLengthChange": false,
+        "bPaginate": false
+    });
+
+
+    $('#ItemsPricesListTable').on('click', 'a.editor_view', function (e) {
+        e.preventDefault();
+        var ItemId = $(this).parents('tr').find('input[name*="ItemId"]')[0].value;
+        location.href = ViewItemPriceRecordUrl + '' + ItemId;
+    });
+});
